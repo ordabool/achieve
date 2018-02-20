c80f7faef2c9438682bfd96f5298972baea1b35f
Or Dabool <or.dabool@gmail.com>
2018-02-20 21:07:14 +0200

    Added basic routing and ejs support + Initialized Angular frontend dir

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const path = require('path');
 
 const port = 3000;
 
+var router = require('./routes/index');
+
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', router);
+
+app.listen(port, function(){
+  console.log('app is running on port ' + port);
+});
