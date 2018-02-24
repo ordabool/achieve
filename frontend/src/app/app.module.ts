122f1f2a3337d6a3789a12ad4cdf821c21d74805
ordabool <or.dabool@gmail.com>
2018-02-24 15:51:06 -0500

    Switched entire frontend to Angular. Included bootstrap & jquery through NPM and .angular-cli config file. Created navbar component using Bootstrap 4.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CreateAchievementComponent } from './components/create-achievement/create-achievement.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAchievementComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/components/create-achievement/create-achievement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-achievement',
+  templateUrl: './create-achievement.component.html',
+  styleUrls: ['./create-achievement.component.css']
+})
+export class CreateAchievementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
