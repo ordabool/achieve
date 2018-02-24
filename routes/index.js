73acc40d357a86c3c46ef87c53788f79c3ab4b43
ordabool <or.dabool@gmail.com>
2018-02-24 12:12:29 -0500

    Changed navbar.ejs to get page title to show as active in the menu

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
-  res.render('pages/index');
+  res.render('pages/home');
+});
+router.get('/dashboard', function(req, res){
+  res.render('pages/dashboard');
 });
 
 module.exports = router;
